677ad40d8e9a9259ade44af0cf2b028388b92c49
MEHMET HAYATİ KINAŞ <34250964+mhkinas@users.noreply.github.com>
2025-04-28 12:55:55 +0300

    Delete JBCA_F directory

--- JBCA_F/src/com/jbca/base/StaticParams.java
+++
@@ -1,8 +0,0 @@
-package com.jbca.base;
-
-public final class StaticParams 
-{
-	public static final String mPLocation = "C:\\Java\\apache\\apache-tomcat-9.0.6\\webapps\\JBCA\\WEB-INF\\tmp";
-		
-	public static final String fileLoc = "C:\\Java\\apache\\apache-tomcat-9.0.6\\webapps\\tmp";
-}
